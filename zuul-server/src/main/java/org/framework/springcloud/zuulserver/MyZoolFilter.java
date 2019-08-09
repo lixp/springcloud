33e6d997c52d4ce28605b27cb326e3bb19348b7f
lixiaopo <lixiaopo@fdc.com.cn>
2019-08-09 18:06:17 +0800

    feat:zuul-routing

---
+++ zuul-server/src/main/java/org/framework/springcloud/zuulserver/MyZoolFilter.java
@@ -0,0 +1,57 @@
+package org.framework.springcloud.zuulserver;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lixp
+ * Date: 2019/8/9
+ * Time: 17:55
+ * Mail: lixp0915@163.com
+ */
+@Service
+public class MyZoolFilter extends ZuulFilter {
+
+    private static Logger log = LoggerFactory.getLogger(MyZoolFilter.class);
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        log.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
+        Object accessToken = request.getParameter("token");
+        if(accessToken == null) {
+            log.warn("token is empty");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            }catch (Exception e){}
+
+            return null;
+        }
+        log.info("ok");
+        return null;
+    }
+}
