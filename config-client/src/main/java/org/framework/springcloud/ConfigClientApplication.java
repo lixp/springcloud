f85648091de4b6a79ec7a0df4215ccdfa7c1e927
lixp <lixp0915@163.com>
2019-08-11 22:56:11 +0800

    feat:msg bus -->broad cast event(e.g status changed event)

--- config-client/src/main/java/org/framework/springcloud/ConfigClientApplication.java
+++ config-client/src/main/java/org/framework/springcloud/ConfigClientApplication.java
@@ -3,12 +3,17 @@ package org.framework.springcloud;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
 @RestController
+@RefreshScope
 public class ConfigClientApplication {
 
 	@Value("${foo}") String foo;
@@ -17,6 +22,8 @@ public class ConfigClientApplication {
 	public String hello() {
 		return foo;
 	}
+
+	//http://localhost:8881/actuator/bus-refresh
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigClientApplication.class, args);
 	}
