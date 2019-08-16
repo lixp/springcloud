5db9160e7895431505d75363c374205538f0143f
lixiaopo <lixiaopo@fdc.com.cn>
2019-08-16 11:38:35 +0800

    feat:hystrix fake combine.

---
+++ service-turbine/src/main/java/org/framework/springcloud/serviceturbine/ServiceTurbineApplication.java
@@ -0,0 +1,15 @@
+package org.framework.springcloud.serviceturbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@SpringBootApplication
+@EnableTurbine
+public class ServiceTurbineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceTurbineApplication.class, args);
+    }
+
+}
